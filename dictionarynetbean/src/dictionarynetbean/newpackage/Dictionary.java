dc1f2564953ab09b245b048002f5fbc48ce19691
traidat <43227093+traidat@users.noreply.github.com>
2018-10-10 22:06:38 +0700

    Add files via upload

---
+++ dictionarynetbean/src/dictionarynetbean/newpackage/Dictionary.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionarynetbean.newpackage;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author VX15
+ */
+public class Dictionary {
+    
+
+    public HashMap<String, String> map = new HashMap<String, String>();
+    
+}
